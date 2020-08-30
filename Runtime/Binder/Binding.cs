ee2f8dcbd3dd3e5e4728198ebcd08b877f296732
Epishev <epishev.m@mail.ru>
2020-08-30 11:30:29 +0300

    Added IBinding

---
+++ Runtime/Binder/Binding.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace EM.Foundation
+{
+	public delegate void Resolver(IBinding binding);
+
+	public class Binding : IBinding
+	{
+		#region IBinding
+
+		public object Key => throw new NotImplementedException();
+
+		public object Name => throw new NotImplementedException();
+
+		public object[] Values => throw new NotImplementedException();
+
+		public IBinding ToSelf()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IBinding To<T>()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IBinding To(object value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IBinding ToName<T>()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IBinding ToName(object name)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IBinding Named<T>()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IBinding Named(object name)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool RemoveValue(object value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void RemoveAllValues()
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+		#region Binding
+
+		public Binding(object key, object name, Resolver resolver)
+		{
+		}
+
+		#endregion
+	}
+}
---
+++ Runtime/Binder/IBinding.cs
@@ -0,0 +1,39 @@
+﻿
+namespace EM.Foundation
+{
+	public interface IBinding
+	{
+		object Key
+		{
+			get;
+		}
+
+		object Name
+		{
+			get;
+		}
+
+		object[] Values
+		{
+			get;
+		}
+
+		IBinding ToSelf();
+
+		IBinding To<T>();
+
+		IBinding To(object value);
+
+		IBinding ToName<T>();
+
+		IBinding ToName(object name);
+
+		IBinding Named<T>();
+
+		IBinding Named(object name);
+
+		bool RemoveValue(object value);
+
+		void RemoveAllValues();
+	}
+}
