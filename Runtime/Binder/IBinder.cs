fe07985c69d02d98199e9f9a5057a6ddef60f1e6
Epishev <epishev.m@mail.ru>
2020-08-30 16:45:28 +0300

    Added IBinder

---
+++ Runtime/Binder/IBinder.cs
@@ -0,0 +1,18 @@
+﻿
+namespace EM.Foundation
+{
+	public interface IBinder
+	{
+		IBinding Bind<T>(object name = null);
+
+		IBinding Bind(object key, object name = null);
+
+		void Unbind<T>(object name = null);
+
+		void Unbind(object key, object name = null);
+
+		IBinding GetBinding<T>(object name = null);
+
+		IBinding GetBinding(object key, object name = null);
+	}
+}
