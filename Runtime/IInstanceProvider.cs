575b17dae18489c6205a2ed1b36ef68f1b604ec8
Maxim Epishev <epishev.m@mail.ru>
2020-09-19 20:59:26 +0300

    Added IInstanceProvider

---
+++ Runtime/IInstanceProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CG.Foundation
+{
+	public interface IInstanceProvider
+	{
+		T GetInstance<T>(object name = null) where T : class;
+
+		object GetInstance(Type key, object name = null);
+	}
+}
