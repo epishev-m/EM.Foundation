a0212bec5d3145815a53b4b3db096333b9e54cda
Maxim Epishev <epishev.m@mail.ru>
2020-09-20 17:13:47 +0300

    Added Signal

---
+++ Runtime/Signal/ISignal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EM.Foundation
+{
+	public interface ISignal
+	{
+		void Dispatch(object[] args);
+
+		void AddListener(Action<ISignal, object[]> action);
+
+		void AddListenerOnce(Action<ISignal, object[]> action);
+
+		void RemoveListener(Action<ISignal, object[]> action);
+
+		void RemoveAllListeners();
+	}
+}
---
+++ Runtime/Signal/Signal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace EM.Foundation
+{
+	public class Signal : ISignal
+	{
+		#region ISignal
+
+		public void Dispatch(object[] args)
+		{
+			_events?.Invoke(this, args);
+		}
+
+		public void AddListener(Action<ISignal, object[]> action)
+		{
+			var unused = action ?? throw new ArgumentNullException(nameof(action));
+			_events += action;
+		}
+
+		public void AddListenerOnce(Action<ISignal, object[]> action)
+		{
+			var unused = action ?? throw new ArgumentNullException(nameof(action));
+			_events += removeAction;
+			_events += action;
+
+			void removeAction(ISignal target, object[] args)
+			{
+				_events -= removeAction;
+				_events -= action;
+			}
+		}
+
+		public void RemoveAllListeners()
+		{
+			_events = null;
+		}
+
+		public void RemoveListener(Action<ISignal, object[]> action)
+		{
+			_events -= action;
+		}
+
+		#endregion
+		#region SignalBase
+
+		protected event Action<ISignal, object[]> _events;
+
+		#endregion
+	}
+}
