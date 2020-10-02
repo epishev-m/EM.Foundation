c036e0b425cac7e646c897eb60fe4d12d1b69dae
Maxim Epishev <epishev.m@mail.ru>
2020-10-02 20:00:25 +0300

    Added SignalEx

---
+++ Runtime/Signal/SignalEx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EM.Foundation
+{
+	public sealed class SignalEx : Signal
+	{
+		public void Dispatch()
+		{
+			Dispatch(null);
+		}
+
+		public void AddListener(Action action)
+		{
+			AddListener(Action);
+
+			void Action(ISignal target, object[] args) => 
+				action?.Invoke();
+		}
+
+		public void AddListenerOnce(Action action)
+		{
+			AddListenerOnce(Action);
+
+			void Action(ISignal target, object[] args) =>
+				action?.Invoke();
+		}
+	}
+}
---
+++ Tests/Runtime/Signal/SignalExTests.cs
@@ -0,0 +1,99 @@
+﻿using EM.Foundation;
+using NUnit.Framework;
+
+internal sealed class SignalExTests
+{
+	[Test]
+	public void SignalEx_AddListenerAndDispatch_Success()
+	{
+		// Arrange
+		var actual = 0;
+		var expected = 1;
+
+		// Act
+		var signal = new SignalEx();
+		signal.AddListener(ActionRuntime);
+		signal.Dispatch();
+
+		void ActionRuntime() => actual++;
+
+		//Assert
+		Assert.AreEqual(expected, actual);
+	}
+
+	[Test]
+	public void SignalEx_AddListenerOnceAndDispatch_Success()
+	{
+		// Arrange
+		var actual = 0;
+		var expected = 1;
+
+		// Act
+		var signal = new SignalEx();
+		signal.AddListenerOnce(ActionRuntime);
+		signal.Dispatch();
+
+		void ActionRuntime() => actual++;
+
+		//Assert
+		Assert.AreEqual(expected, actual);
+	}
+
+	[Test]
+	public void SignalEx_AddListenerAnd2Dispatch_Success()
+	{
+		// Arrange
+		var expected = 2;
+		var actual = 0;
+
+		// Act
+		var signal = new SignalEx();
+		signal.AddListener(ActionRuntime);
+		signal.Dispatch();
+		signal.Dispatch();
+
+		void ActionRuntime() => actual++;
+
+		//Assert
+		Assert.AreEqual(expected, actual);
+	}
+
+	[Test]
+	public void SignalEx_AddListenerOnceAnd2Dispatch_Success()
+	{
+		// Arrange
+		var expected = 1;
+		var actual = 0;
+
+		// Act
+		var signal = new SignalEx();
+		signal.AddListenerOnce(ActionRuntime);
+		signal.Dispatch();
+		signal.Dispatch();
+
+		void ActionRuntime() => actual++;
+
+		//Assert
+		Assert.AreEqual(expected, actual);
+	}
+
+	[Test]
+	public void SignalEx_RemoveAllListeners_Success()
+	{
+		// Arrange
+		var expected = 0;
+		var actual = 0;
+
+		// Act
+		var signal = new SignalEx();
+		signal.AddListener(ActionRuntime);
+		signal.AddListenerOnce(ActionRuntime);
+		signal.RemoveAllListeners();
+		signal.Dispatch();
+
+		void ActionRuntime() => actual++;
+
+		//Assert
+		Assert.AreEqual(expected, actual);
+	}
+}
