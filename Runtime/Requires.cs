5216fc2e61345c5d20312b79f8eb65f958037f83
Maxim Epishev <epishev.m@mail.ru>
2020-11-02 22:42:36 +0300

    Added StringResources

--- Runtime/Requires.cs
+++ Runtime/Requires.cs
@@ -2,12 +2,15 @@
 namespace EM.Foundation
 {
 	using System;
+	using System.Reflection;
 	using System.Runtime.CompilerServices;
 
 	public static class Requires
 	{
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static void IsNotNull(object instance, string paramName)
+		public static void IsNotNull(
+			object instance,
+			string paramName)
 		{
 			if (instance is null)
 			{
@@ -15,9 +18,55 @@ namespace EM.Foundation
 			}
 		}
 
-		private static void ThrowArgumentNullException(string paramName)
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void IsNull(
+			object instance,
+			string paramName)
+		{
+			if ((instance is null) == false)
+			{
+				ThrowArgumentException(paramName);
+			}
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void IsReferenceType(
+			Type type,
+			string paramName)
+		{
+			var typeInfo = type.GetTypeInfo();
+
+			if (!typeInfo.IsClass && !typeInfo.IsInterface)
+			{
+				ThrowArgumentException(paramName,
+					StringResources.SuppliedTypeIsNotAReferenceType(type));
+			}
+		}
+
+		private static void ThrowArgumentNullException(
+			string paramName)
 		{
 			throw new ArgumentNullException(paramName);
+		}
+
+		private static void ThrowArgumentNullException(
+			string paramName,
+			string message)
+		{
+			throw new ArgumentNullException(paramName, message);
+		}
+
+		private static void ThrowArgumentException(
+			string paramName)
+		{
+			throw new ArgumentException(paramName);
+		}
+
+		private static void ThrowArgumentException(
+			string paramName,
+			string message)
+		{
+			throw new ArgumentException(paramName, message);
 		}
 	}
 }
---
+++ Runtime/StringResources.cs
@@ -0,0 +1,16 @@
+﻿
+namespace EM.Foundation
+{
+	using System;
+	using System.Globalization;
+
+	internal static class StringResources
+	{
+		internal static string SuppliedTypeIsNotAReferenceType(Type type)
+		{
+			return string.Format(CultureInfo.InvariantCulture,
+				"The supplied type {0} is not a reference type. Only reference types are supported.",
+					type);
+		}
+	}
+}
