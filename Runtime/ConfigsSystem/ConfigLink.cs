bdff0638a35440e71a8ef8b3a7af3f5cc2b3be48
Maxim Epishev <epishev.m@mail.ru>
2022-03-08 20:27:45 +0300

    Added ConfigSystems

---
+++ Runtime/ConfigsSystem/ConfigLink.cs
@@ -0,0 +1,33 @@
+﻿
+namespace EM.Foundation
+{
+using System;
+
+[Serializable]
+public abstract class ConfigLink
+{
+	public readonly Type Type;
+
+	public readonly string Name;
+
+	protected ConfigLink(
+		Type entryType,
+		string name)
+	{
+		Type = entryType;
+		Name = name;
+	}
+}
+
+[Serializable]
+public sealed class ConfigLink<T> :
+	ConfigLink
+{
+	public ConfigLink(
+		string name) :
+		base(typeof(T), name)
+	{
+	}
+}
+
+}
---
+++ Runtime/ConfigsSystem/UnionAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EM.Foundation
+{
+
+[AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class,
+	AllowMultiple = true,
+	Inherited = false)]
+public sealed class UnionAttribute :
+	Attribute
+{
+	public Type Type { get; }
+
+	public UnionAttribute(Type type)
+	{
+		Type = type;
+	}
+}
+
+}
