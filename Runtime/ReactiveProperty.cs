163950123d0e06cfcfdb800b692a14d5d45e945c
Maxim Epishev <epishev.m@mail.ru>
2022-03-08 20:29:03 +0300

    Added ReactiveProperty

---
+++ Runtime/ReactiveProperty.cs
@@ -0,0 +1,45 @@
+﻿namespace EM.Foundation
+{
+using System;
+
+public sealed class ReactiveProperty<T>
+{
+	private T value;
+
+	private event Action OnChanged;
+
+	#region ReactiveProperty
+
+	public T Value
+	{
+		get => value;
+		set
+		{
+			if (Equals(value, this.value))
+			{
+				return;
+			}
+
+			this.value = value;
+			OnChanged?.Invoke();
+		}
+	}
+
+	public void Subscribe(Action handler)
+	{
+		Requires.NotNull(handler, nameof(handler));
+
+		OnChanged += handler;
+	}
+
+	public void UnSubscribe(Action handler)
+	{
+		Requires.NotNull(handler, nameof(handler));
+
+		OnChanged -= handler;
+	}
+	
+	#endregion
+}
+
+}
