28acc078d808c48012a9cb42f37fca9c377017fe
Maxim Epishev <epishev.m@mail.ru>
2022-03-08 20:28:47 +0300

    Added AssetsManager

---
+++ Runtime/AssetsManager/AssetsManager.cs
@@ -0,0 +1,60 @@
+﻿namespace EM.Foundation
+{
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public sealed class AssetsManager :
+	IAssetsManager
+{
+	#region IAssetsManager
+
+	public async Task<GameObject> InstantiateAsync(string path,
+		Transform parent = null)
+	{
+		var handle = parent == null 
+			? Addressables.InstantiateAsync(path)
+			: Addressables.InstantiateAsync(path, parent);
+		var gameObject = await handle.Task;
+
+		if (handle.Status != AsyncOperationStatus.Succeeded)
+		{
+			return default;
+		}
+
+		gameObject.name = path;
+
+		return gameObject;
+	}
+
+	public async Task<T> InstantiateAsync<T>(string path,
+		Transform parent = null)
+		where T : Component
+	{
+		var gameObject = await InstantiateAsync(path, parent);
+
+		if (gameObject.TryGetComponent(out T component) == false)
+		{
+			throw new NullReferenceException(
+				$"Object of type {typeof(T)} is null on attempt to load it from addressables");
+		}
+
+		return component;
+	}
+
+	public bool ReleaseInstance(GameObject gameObject)
+	{
+		Requires.NotNull(gameObject, nameof(gameObject));
+
+		gameObject.SetActive(false);
+		var result = Addressables.ReleaseInstance(gameObject);
+
+		return result;
+	}
+
+	#endregion
+}
+
+}
---
+++ Runtime/AssetsManager/IAssetsManager.cs
@@ -0,0 +1,18 @@
+﻿namespace EM.Foundation
+{
+using System.Threading.Tasks;
+using UnityEngine;
+
+public interface IAssetsManager
+{
+	Task<GameObject> InstantiateAsync(string path,
+		Transform parent = null);
+
+	Task<T> InstantiateAsync<T>(string path,
+		Transform parent = null)
+		where T : Component;
+
+	bool ReleaseInstance(GameObject gameObject);
+}
+
+}
