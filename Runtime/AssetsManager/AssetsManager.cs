32cd6af084757dc0f004330316da72d74ec556fd
Maxim Epishev <epishev.m@mail.ru>
2022-06-19 23:23:28 +0300

    Refactoring: AssetsManager

--- Runtime/AssetsManager/AssetsManager.cs
+++ Runtime/AssetsManager/AssetsManager.cs
@@ -1,10 +1,9 @@
-﻿using Cysharp.Threading.Tasks;
-
-namespace EM.Foundation
+﻿namespace EM.Foundation
 {
 
+using Cysharp.Threading.Tasks;
 using System;
-using System.Threading.Tasks;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
@@ -15,15 +14,24 @@ public sealed class AssetsManager :
 	#region IAssetsManager
 
 	public async UniTask<GameObject> InstantiateAsync(string path,
-		Transform parent = null)
+		CancellationToken ct)
 	{
-		var handle = parent == null
+		return await InstantiateAsync(path, null, ct);
+	}
+
+	public async UniTask<GameObject> InstantiateAsync(string path,
+		Transform parent,
+		CancellationToken ct)
+	{
+		ct.ThrowIfCancellationRequested();
+
+		var handler = parent == null
 			? Addressables.InstantiateAsync(path)
 			: Addressables.InstantiateAsync(path, parent);
 
-		var gameObject = await handle.Task;
+		var gameObject = await handler.Task;
 
-		if (handle.Status != AsyncOperationStatus.Succeeded)
+		if (handler.Status != AsyncOperationStatus.Succeeded)
 		{
 			return default;
 		}
@@ -34,10 +42,18 @@ public sealed class AssetsManager :
 	}
 
 	public async UniTask<T> InstantiateAsync<T>(string path,
-		Transform parent = null)
+		CancellationToken ct)
 		where T : Component
 	{
-		var gameObject = await InstantiateAsync(path, parent);
+		return await InstantiateAsync<T>(path, null, ct);
+	}
+
+	public async UniTask<T> InstantiateAsync<T>(string path,
+		Transform parent,
+		CancellationToken ct)
+		where T : Component
+	{
+		var gameObject = await InstantiateAsync(path, parent, ct);
 
 		if (gameObject.TryGetComponent(out T component) == false)
 		{
--- Runtime/AssetsManager/IAssetsManager.cs
+++ Runtime/AssetsManager/IAssetsManager.cs
@@ -3,16 +3,24 @@
 namespace EM.Foundation
 {
 
-using System.Threading.Tasks;
+using System.Threading;
 using UnityEngine;
 
 public interface IAssetsManager
 {
 	UniTask<GameObject> InstantiateAsync(string path,
-		Transform parent = null);
+		CancellationToken ct);
+	
+	UniTask<GameObject> InstantiateAsync(string path,
+		Transform parent, CancellationToken ct);
 
 	UniTask<T> InstantiateAsync<T>(string path,
-		Transform parent = null)
+		CancellationToken ct)
+		where T : Component;
+
+	UniTask<T> InstantiateAsync<T>(string path,
+		Transform parent,
+		CancellationToken ct)
 		where T : Component;
 
 	bool ReleaseInstance(GameObject gameObject);
