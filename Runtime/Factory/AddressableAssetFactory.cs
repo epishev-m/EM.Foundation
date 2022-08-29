d3f150714bc712be72339f53c7703b7b21283ab5
Maxim Epishev <epishev.m@mail.ru>
2022-08-30 01:56:56 +0300

    Added AddressableAssetFactory

---
+++ Runtime/Factory/AddressableAssetFactory.cs
@@ -0,0 +1,40 @@
+﻿namespace EM.Foundation
+{
+
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public abstract class AddressableAssetFactory<T> : IFactory
+	where T : ScriptableObject
+{
+	#region IFactory
+
+	public bool TryCreate(out object instance)
+	{
+		var operationHandle = Addressables.LoadAssetAsync<T>(Path);
+		var config = operationHandle.WaitForCompletion();
+		instance = config;
+
+		if (config == null)
+		{
+			return false;
+		}
+
+		Addressables.Release(config);
+
+		return true;
+	}
+
+	#endregion
+
+	#region AddressableAssetFactory
+
+	protected abstract string Path
+	{
+		get;
+	}
+
+	#endregion
+}
+
+}
