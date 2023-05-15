cfa3cbf63ac5731a322ca5dfc98a35391ccef1cb
Maxim Epishev <epishev.m@mail.ru>
2023-05-16 01:45:37 +0300

    Added LocalContainerPool

---
+++ Runtime/Pool/LocalContainerPool.cs
@@ -0,0 +1,70 @@
+﻿namespace EM.Game
+{
+
+using System;
+using System.Collections.Concurrent;
+using Foundation;
+using UnityEngine;
+
+public class LocalContainerPool<T> : IPool<T>
+	where T : MonoBehaviour
+{
+	private readonly ConcurrentBag<T> _instances = new();
+
+	#region IPool
+
+	public int Count => _instances.Count;
+
+	public Result<T> GetObject()
+	{
+		if (_instances.TryTake(out var obj))
+		{
+			obj.gameObject.SetActive(true);
+
+			return new SuccessResult<T>(obj);
+		}
+
+		return new ErrorResult<T>("No elements available");
+	}
+
+	public Result PutObject(T obj)
+	{
+		if (obj == null)
+		{
+			return new ErrorResult(PoolStringResources.PutNullObject(this));
+		}
+
+		if (obj is IPoolable {IsRestored: false} poolItem)
+		{
+			poolItem.Restore();
+		}
+
+		if (obj is IDisposable disposable)
+		{
+			disposable.Dispose();
+		}
+
+		obj.gameObject.SetActive(false);
+		_instances.Add(obj);
+
+		return new SuccessResult();
+	}
+
+	#endregion
+
+	#region LocalContainerPool
+
+	public LocalContainerPool(Component container)
+	{
+		var objects = container.GetComponentsInChildren<T>(true);
+
+		foreach (var obj in objects)
+		{
+			PutObject(obj);
+		}
+	}
+
+	#endregion
+}
+
+}
